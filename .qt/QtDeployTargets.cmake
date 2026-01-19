df7ac392dfbaecf1cc2f45e05da1746279f8d93f
Simon Vogt <simon.vogt@planemos.de>
2026-01-19 10:26:18 +0100

    Initial commit: Qt HTTP Server Projekt

---
+++ .qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_simple_FILE C:/Users/simon.vogt/Documents/2_schule/IFA12C/Projekt/Test/simple.exe)
+set(__QT_DEPLOY_TARGET_simple_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_simple_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6HttpServer.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6WebSockets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
