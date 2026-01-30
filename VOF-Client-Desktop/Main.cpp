31ebcbe6eb93b8aa5b218f06acbe29fde038a1ea
Marcos Hebestreit <marcos.freire-pinto-hebestreit@bs-erlangen.de>
2026-01-30 14:35:15 +0100

    commit bevore stash 2

--- VOF-Client-Desktop/Main.cpp
+++ VOF-Client-Desktop/Main.cpp
@@ -7,21 +7,20 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
     a.setStyle(QStyleFactory::create("Fusion"));
     a.setQuitOnLastWindowClosed(false);
 
-    QPalette palette;
-    palette.setColor(QPalette::Window, Qt::white);
-    palette.setColor(QPalette::WindowText, Qt::black);
-    palette.setColor(QPalette::Base, Qt::white);
-    palette.setColor(QPalette::AlternateBase, QColor(240, 240, 240));
-    palette.setColor(QPalette::Text, Qt::black);
-    palette.setColor(QPalette::Button, QColor(240, 240, 240));
-    palette.setColor(QPalette::ButtonText, Qt::black);
-    palette.setColor(QPalette::Highlight, QColor(0, 120, 215));
-    palette.setColor(QPalette::HighlightedText, Qt::white);
-
+    QPalette palette;{
+        palette.setColor(QPalette::Window, Qt::white);
+        palette.setColor(QPalette::WindowText, Qt::black);
+        palette.setColor(QPalette::Base, Qt::white);
+        palette.setColor(QPalette::AlternateBase, QColor(240, 240, 240));
+        palette.setColor(QPalette::Text, Qt::black);
+        palette.setColor(QPalette::Button, QColor(240, 240, 240));
+        palette.setColor(QPalette::ButtonText, Qt::black);
+        palette.setColor(QPalette::Highlight, QColor(0, 120, 215));
+        palette.setColor(QPalette::HighlightedText, Qt::white);
+    }
     a.setPalette(palette);
 
     QString profile = (argc > 1) ? argv[1] : "DefaultPlayer";
